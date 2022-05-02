8051ec53fb74cbd4b5a726799222f2d7272036c2
p-artie <artiepolanco@gmail.com>
2022-05-02 17:14:43 -0400

    Create the basic midi player

---
+++ src/app/MiniMiniMusicApp.java
@@ -0,0 +1,38 @@
+package app;
+import javax.sound.midi.MidiEvent;
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.Sequence;
+import javax.sound.midi.Sequencer;
+import javax.sound.midi.ShortMessage;
+import javax.sound.midi.Track;
+
+public class MiniMiniMusicApp {
+	public static void main(String[] args) {
+		MiniMiniMusicApp mini = new MiniMiniMusicApp();
+		mini.play();
+	}
+
+	public void play() {
+		try {
+			Sequencer player = MidiSystem.getSequencer();
+			player.open();
+			Sequence seq = new Sequence(Sequence.PPQ, 4);
+			Track track = seq.createTrack();
+			
+			ShortMessage a = new ShortMessage();
+			a.setMessage(144, 1, 50, 100);
+			MidiEvent noteOn = new MidiEvent(a, 1);
+			track.add(noteOn);
+			
+			ShortMessage b = new ShortMessage();
+			b.setMessage(128, 1, 50, 100);
+			MidiEvent noteoff = new MidiEvent(b, 6);
+			track.add(noteoff);
+			
+			player.setSequence(seq);
+			player.start();
+		} catch (Exception ex){
+			ex.printStackTrace();
+		}
+	}
+}
