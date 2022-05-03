94c7f555a3ce84454a0c2826b7682d2117baa9e0
p-artie <artiepolanco@gmail.com>
2022-05-03 15:45:36 -0400

    Build an animation GUI with JFrame

---
+++ src/app/SimpleAnimation.java
@@ -0,0 +1,47 @@
+package app;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class SimpleAnimation {
+	int x = 70;
+	int y = 70;
+	
+	public static void main(String[] args) {
+		SimpleAnimation gui = new SimpleAnimation();
+		gui.go();
+	}
+	
+	public void go() {
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		MyDrawPanel drawPanel = new MyDrawPanel();
+		
+		frame.getContentPane().add(drawPanel);
+		frame.setSize(500, 500);
+		frame.setVisible(true);
+		
+		for (int i = 0; i <130; i++) {
+			x++;
+			y++;
+			
+			drawPanel.repaint();
+			
+			try {
+				Thread.sleep(50);
+			} catch (Exception ex) {}
+		}
+	}
+	
+	@SuppressWarnings("serial")
+	class MyDrawPanel extends JPanel {
+		public void paintComponent(Graphics g) {
+			g.setColor(Color.green);
+			g.fillOval(x, y, 40, 40);
+		}
+	}
+}
