6139522596477590539c32d9147a690063e170b3
p-artie <artiepolanco@gmail.com>
2022-05-04 16:27:31 -0400

    Add 3 buttons to JPanel

---
+++ src/app/Panel1.java
@@ -0,0 +1,33 @@
+package app;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Panel1 {
+
+	public static void main(String[] args) {
+		Panel1 gui = new Panel1();
+		gui.go();
+	}
+	
+	public void go() {
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.darkGray);
+		JButton button = new JButton("shock me");
+		JButton button2 = new JButton("Bliss");
+		JButton button3 = new JButton("huh?");
+		
+		panel.add(button);
+		panel.add(button2);
+		panel.add(button3);
+		frame.getContentPane().add(BorderLayout.EAST, panel);
+		frame.setSize(200, 200);
+		frame.setVisible(true);
+	}
+
+}
